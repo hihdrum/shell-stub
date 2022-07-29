a707c816baebf280d3e83a5c6c65161594e7739a
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-29 22:40:32 +0900

    1引数の保存処理を実装した。

--- a.sh
+++ a.sh
@@ -9,6 +9,13 @@ expectOutputFile="a.expectOutput${callcountStr}"
 # 戻り値
 expectFile="a.expect${callcountStr}"
 
+# 呼び出し引数の履歴
+historyFile="a.history${callcountStr}.arg1"
+
+# 呼び出し引数の保存
+echo "$1" > ${historyFile}
+
+
 returnValue=$(cat ${expectFile})
 
 # 実行時の出力
