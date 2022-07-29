22bd0e817d769b1d956a427869a0a2b8968377f9
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-29 22:33:18 +0900

    実行時の出力を指定できるようにした。

--- a.sh
+++ a.sh
@@ -3,8 +3,16 @@
 callcount=$(cat a.callcount)
 callcountStr=$(echo ${callcount} | awk '{printf("%03d", $0 + 1)}')
 
+# 実行時の出力
+expectOutputFile="a.expectOutput${callcountStr}"
+
+# 戻り値
 expectFile="a.expect${callcountStr}"
+
 returnValue=$(cat ${expectFile})
+
+# 実行時の出力
+cat ${expectOutputFile}
 
 # 情報の更新
 echo $((${callcount} + 1)) > a.callcount
