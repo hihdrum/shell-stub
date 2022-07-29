e1cc60d91b101fbaaea22e88ae87589495afc5d7
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-29 22:22:35 +0900

    呼び出し回数に応じて次の期待値ファイルを読み込むようにした。

--- a.sh
+++ a.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+callcount=$(cat a.callcount)
+callcountStr=$(echo ${callcount} | awk '{printf("%03d", $0 + 1)}')
 
-callcount=$(cat a.callcount | awk '{printf("%03d", $0 + 1)}')
-
-expectFile="a.expect${callcount}"
+expectFile="a.expect${callcountStr}"
 returnValue=$(cat ${expectFile})
 
+# 情報の更新
+echo $((${callcount} + 1)) > a.callcount
+
+# 結果
 exit ${returnValue}
