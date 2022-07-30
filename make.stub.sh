4c632465cbbb3381898f249e683063998179b126
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-30 20:22:31 +0900

    シェル名に自動で.shを付与しないようにした。

--- make.stub.sh
+++ make.stub.sh
@@ -4,13 +4,13 @@ shellName=$1
 
 dirPath=$(dirname $0)
 
-cat ${dirPath}/template.sh | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.sh
+cat ${dirPath}/template.sh | sed "s/SHELLNAME/${shellName}/g" > ${shellName}
 cat ${dirPath}/template.clear.callcount | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.clear.callcount
 cat ${dirPath}/template.set.expect | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.set.expect
 cat ${dirPath}/template.set.expectOutput | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.set.expectOutput
 cat ${dirPath}/template.reset | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.reset
 
-chmod u+x ${shellName}.sh ${shellName}.clear.callcount \
+chmod u+x ${shellName} ${shellName}.clear.callcount \
           ${shellName}.set.expect ${shellName}.set.expectOutput ${shellName}.reset
 
 echo 0 > ${shellName}.callcount
