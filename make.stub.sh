e38062310d7bb7dcf623e7e2c9d6fcb113517684
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-31 15:23:30 +0900

    期待戻り値と出力を設定するスクリプトをまとめた。

--- make.stub.sh
+++ make.stub.sh
@@ -6,13 +6,12 @@ dirPath=$(dirname $0)
 
 cat ${dirPath}/template.sh | sed "s/SHELLNAME/${shellName}/g" > ${shellName}
 cat ${dirPath}/template.clear.callcount | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.clear.callcount
-cat ${dirPath}/template.set.expect.return | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.set.expect.return
-cat ${dirPath}/template.set.expect.output | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.set.expect.output
+cat ${dirPath}/template.set.expect | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.set.expect
 cat ${dirPath}/template.reset | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.reset
 cat ${dirPath}/template.rm.history | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.rm.history
 
 chmod u+x ${shellName} ${shellName}.clear.callcount \
-          ${shellName}.set.expect.return ${shellName}.set.expect.output ${shellName}.reset \
+          ${shellName}.set.expect ${shellName}.reset \
           ${shellName}.rm.history
 
 echo 0 > ${shellName}.callcount
