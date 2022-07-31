18f934749815a0f98792fc0a7db3bdaa80ba634d
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-31 18:02:53 +0900

    make.stub.shのテストを追加した。

---
+++ test/make.stub.sh_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 本ディレクトリに shunit2 を用意して下さい。
+
+oneTimeSetUp() {
+  rm -rf stub
+  mkdir stub
+  pushd stub > /dev/null
+  ../../make.stub.sh sample.sh
+  popd > /dev/null
+}
+
+test_Make_shell() {
+  assertTrue "[ -x stub/sample.sh ]"
+}
+
+test_Make_clearCallcount() {
+  assertTrue "[ -x stub/sample.sh.clear.callcount ]"
+}
+
+test_Make_setExpect() {
+  assertTrue "[ -x stub/sample.sh.set.expect ]"
+}
+
+test_Make_init() {
+  assertTrue "[ -x stub/sample.sh.init ]"
+}
+
+test_Make_rmHistory() {
+  assertTrue "[ -x stub/sample.sh.rm.history ]"
+}
+
+test_Make_callcount() {
+  assertTrue "[ -r stub/sample.sh.callcount ]"
+  assertEquals 0 $(cat stub/sample.sh.callcount)
+}
+
+# Load shuUnit2.
+. ./shunit2
+
