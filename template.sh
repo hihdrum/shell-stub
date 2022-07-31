2b7a03b8d0a37cfd72fee6a1aa7124fc578135ef
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-31 15:47:50 +0900

    コメントの追加し、変数を調整した。

--- template.sh
+++ template.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+#------------------------------------------------------------
+# スタブシェル
+#   呼び出された回数に応じて、設定した戻り値の返却と出力を行う。
+#   呼び出された際の引数の個数と各引数をファイルに保持する。
+#
+#  呼び出し引数 : 任意
+#------------------------------------------------------------
 
 stubDir=$(dirname $0)
 
-# スクリプト呼び出し回数情報
+# 呼び出し回数情報
 callcount=$(cat ${stubDir}/SHELLNAME.callcount)
 callcountStr=$(echo ${callcount} | awk '{printf("%03d", $0 + 1)}')
 
-# 実行時の出力
-expectOutputFile="${stubDir}/SHELLNAME.expect.output${callcountStr}"
-
-# 戻り値
-expectFile="${stubDir}/SHELLNAME.expect.return${callcountStr}"
-
-# 呼び出し引数の履歴保存
+#----------------------------------------
+# 引数情報の保存
+#----------------------------------------
+# 個数情報
 echo $# > "SHELLNAME.history${callcountStr}.argc"
 
+# 各引数の情報
 argIndex=1
 for arg in "$@"
 do
@@ -26,21 +31,31 @@ do
 
 done
 
-returnValue=$(cat ${expectFile})
+#----------------------------------------
+# 実行時の戻り値取得と出力
+#----------------------------------------
+# 戻り値
+expectReturnFile="${stubDir}/SHELLNAME.expect.return${callcountStr}"
+returnValue=$(cat ${expectReturnFile})
 if [ $? -ne 0 ]; then
-  echo "戻り値を設定するファイル(${expectFile}) がcatできませんでした。" > /dev/stderr
+  echo "戻り値を設定するファイル(${expectReturnFile}) がcatできませんでした。" > /dev/stderr
   exit 255
 fi
 
-# 実行時の出力
+# 出力
+expectOutputFile="${stubDir}/SHELLNAME.expect.output${callcountStr}"
 cat ${expectOutputFile}
 if [ $? -ne 0 ]; then
   echo "出力を設定するファイル(${expectOutputFile}) がcatできませんでした。" > /dev/stderr
   exit 254
 fi
 
+#----------------------------------------
 # 情報の更新
+#----------------------------------------
+# 呼び出し回数情報
 echo $((${callcount} + 1)) > ${stubDir}/SHELLNAME.callcount
 
-# 結果
+# 戻り値
 exit ${returnValue}
+
