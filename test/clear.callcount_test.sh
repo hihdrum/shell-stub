5b9e7feb1d45353e4f2ba0e4b9c081f8c15831e8
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-31 18:15:43 +0900

    スタブディレクトリをプロセスIDで分けることにした。shunitの呼び出し後にTearDownを用意することにした。

--- test/clear.callcount_test.sh
+++ test/clear.callcount_test.sh
@@ -3,27 +3,27 @@
 # 本ディレクトリに shunit2 を用意して下さい。
 
 oneTimeSetUp() {
-  rm -rf stub
-  mkdir stub
-  pushd stub > /dev/null
+  mkdir stub.$$
+  pushd stub.$$ > /dev/null
   ../../make.stub.sh sample.sh
-  popd > /dev/null
-
   PATH=$(pwd):$PATH
+  popd > /dev/null
 }
 
 test_callcount_0To0() {
-  echo 0 > stub/sample.sh.callcount
+  echo 0 > stub.$$/sample.sh.callcount
   sample.sh.clear.callcount
-  assertEquals 0 $(cat stub/sample.sh.callcount)
+  assertEquals 0 $(cat stub.$$/sample.sh.callcount)
 }
 
 test_callcount_1To0() {
-  echo 1 > stub/sample.sh.callcount
+  echo 1 > stub.$$/sample.sh.callcount
   sample.sh.clear.callcount
-  assertEquals 0 $(cat stub/sample.sh.callcount)
+  assertEquals 0 $(cat stub.$$/sample.sh.callcount)
 }
 
 # Load shuUnit2.
 . ./shunit2
 
+# テストスクリプトのTearDown
+rm -rf stub.$$
