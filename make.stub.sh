b7f9ebe1429c24f0e8f4048445c6df2a867b2a57
makoto <88492711+hihdrum@users.noreply.github.com>
2022-07-31 15:55:43 +0900

    resetをinitに変更した。

--- make.stub.sh
+++ make.stub.sh
@@ -7,11 +7,11 @@ dirPath=$(dirname $0)
 cat ${dirPath}/template.sh | sed "s/SHELLNAME/${shellName}/g" > ${shellName}
 cat ${dirPath}/template.clear.callcount | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.clear.callcount
 cat ${dirPath}/template.set.expect | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.set.expect
-cat ${dirPath}/template.reset | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.reset
+cat ${dirPath}/template.init | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.init
 cat ${dirPath}/template.rm.history | sed "s/SHELLNAME/${shellName}/g" > ${shellName}.rm.history
 
 chmod u+x ${shellName} ${shellName}.clear.callcount \
-          ${shellName}.set.expect ${shellName}.reset \
+          ${shellName}.set.expect ${shellName}.init \
           ${shellName}.rm.history
 
 echo 0 > ${shellName}.callcount
